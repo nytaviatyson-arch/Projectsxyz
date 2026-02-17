a360a854c68f6a76373ce021ba016bc184a3690f
nytaviatyson-arch <nytavia.tyson@gmail.com>
2026-02-16 19:06:52 -0600

    Add files via upload

---
+++ First.java
@@ -0,0 +1,12 @@
+// Nytavia Tyson
+public class First {
+public static void main(String[] args) {
+        String jobTitle = "Computer Support Specialist";
+        String description = "Great career-starter job!";
+        String salary = "Median salary: $61,550 per year.";
+
+        System.out.println("Job Title: " + jobTitle);
+        System.out.println(description);
+        System.out.println(salary);
+    }
+}
