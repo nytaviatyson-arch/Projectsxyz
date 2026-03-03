5f8c79c53b6501940346f8d4850deee8222e71f4
nytaviatyson-arch <nytavia.tyson@gmail.com>
2026-03-02 18:32:34 -0600

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,19 @@
+public class Animal {
+    private String species;
+
+    public Animal() {
+        this.species = species;
+    }
+    public Animal (String newSpecies) {
+        this.species = newSpecies;
+    }
+    public void setSpecies(String newSpecies) {
+        this.species = newSpecies;
+    }
+    public String getSpecies() {
+        return this.species;
+    }
+    public String toString() {
+        return this.species;
+    }    
+}
--- Runner.java
+++ Runner.java
@@ -1,30 +1,12 @@
-import java.util.Scanner;
-
 public class Runner {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-     
-        Pet2 pet0 = new Pet2();
-        System.out.println("pet0");
-        System.out.println(pet0);
+        
+        Animal a1 = new Animal();
+        a1.setSpecies("Dog");
+        System.out.println(a1.getSpecies());
 
-        Pet2 pet1 = new Pet2("Dumbo", "Elephant", 10);
-        System.out.println("pet1");
-        System.out.println(pet1);
-
-        Pet2 pet2 = new Pet2("Ally", "Wolf", 5);
-        System.out.println("pet2");
-        System.out.println(pet2);
-
-        System.out.println("Enter a pet name:");
-        String name = scanner.nextLine();
-        System.out.println("Enter a pet type:");
-        String type = scanner.nextLine();
-        System.out.println("Enter a pet age:");
-        int age = scanner.nextInt();
-        Pet2 pet4 = new Pet2(name, type, age);
-        System.out.println("pet4");
-
-        scanner.close();
+        Animal a2 = new Animal();
+        a2.setSpecies("Cat");
+        System.out.println(a2.getSpecies());
     }
 }
